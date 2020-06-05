39cc14d1e6db6a5cf5484472dcbcfec4f9a0b2fc
samyounger <sam.younger@gmail.com>
2020-06-06 00:23:52 +0100

    template login-register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '@/_services';
+import { User } from '@/_models';
+
+import '@/_content/app.scss';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app',
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   title = 'samyounger-admin';
+  currentUser: User;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+    ) {
+      this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-// Coponents
-import { LoginComponent } from './login/login.component';
+// used to create fake backend
+import { fakeBackendProvider } from '@/_helpers';
+
+import { appRoutingModule } from './app.routing';
+import { JwtInterceptor, ErrorInterceptor } from '@/_helpers';
+import { AppComponent } from '@/app.component';
+import { HomeComponent } from '@/home';
+import { LoginComponent } from '@/login';
+import { RegisterComponent } from '@/register';
+import { AlertComponent } from '@/_components';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-  ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: 'login', component: LoginComponent },
-    ])
+    HttpClientModule,
+    appRoutingModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AlertComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { };
